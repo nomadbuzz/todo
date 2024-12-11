13b7c782040622a1596f764482f521e98db19b19
nomadbuzz <baz.coding@gmail.com>
2024-12-11 13:58:51 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ToDoList from './component/ToDoList'
+
+
+const App = () => {
+  return (
+    <div className='bg-stone-900 grid py-4 min-h-screen'>
+        <ToDoList/>
+    </div>
+  )
+}
+
+export default App 
